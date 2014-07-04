1d52c145323f5d45c97fee866826d0dc3a3e3d59
SaschaNaz <saschanaz@outlook.com>
2014-07-04 17:25:40 +0900

    mathextension submodule

---
+++ matrixcsv.d.ts
@@ -0,0 +1,5 @@
+﻿declare module CSV {
+    function readFile(file: Blob, onload: (matrix: Matrix<string>) => any, delimiter?: string): void;
+    function decode(text: string, delimiter?: string): Matrix<string>;
+    function encode(matrix: Matrix<any>): string;
+}
---
+++ matrixcsv.js
@@ -0,0 +1,106 @@
+﻿var CSV;
+(function (CSV) {
+    function readFile(file, onload, delimiter) {
+        if (typeof delimiter === "undefined") { delimiter = ','; }
+        var reader = new FileReader();
+        reader.onload = function (ev) {
+            onload(CSV.decode(ev.target.result, delimiter));
+        };
+        reader.readAsText(file);
+    }
+    CSV.readFile = readFile;
+    function decode(text, delimiter) {
+        if (typeof delimiter === "undefined") { delimiter = ','; }
+        var matrix = new Matrix();
+        var currentPosition = 0;
+        var linePosition = 1;
+        while (currentPosition < text.length) {
+            var lineObject = readLine(text, currentPosition);
+            if (!lineObject)
+                break;
+            currentPosition = lineObject.nextPosition;
+
+            var splitted = splitByDelimiter(lineObject.line, delimiter);
+            for (var i = 1; i <= splitted.length; i++) {
+                matrix.set([linePosition, i], splitted[i - 1]);
+            }
+            linePosition++;
+        }
+        return matrix;
+    }
+    CSV.decode = decode;
+    function readLine(str, startIndex) {
+        var found = str.indexOf('\n', startIndex);
+        if (found != -1)
+            return { line: str.slice(startIndex, found), nextPosition: found + 1 };
+        else
+            return { line: str.slice(startIndex), nextPosition: str.length };
+    }
+    function splitByDelimiter(str, delimiter) {
+        var isInQuote = false;
+        var items = [];
+        var item = "";
+        var mayQuoteEnd = false;
+
+        for (var i = 0; i < str.length; i++) {
+            var char = str[i];
+
+            //delimiter
+            if (char === delimiter && !(isInQuote && !mayQuoteEnd)) {
+                items.push(item);
+                item = "";
+                isInQuote = false;
+                mayQuoteEnd = false;
+                continue;
+            }
+
+            //quote escaping
+            if (char === '"') {
+                if (item.length == 0) {
+                    isInQuote = true;
+                    continue;
+                } else if (isInQuote) {
+                    if (!mayQuoteEnd) {
+                        mayQuoteEnd = true;
+                        continue;
+                    } else
+                        mayQuoteEnd = false;
+                }
+            } else if (mayQuoteEnd)
+                mayQuoteEnd = false;
+
+            item += char;
+        }
+
+        return items;
+    }
+
+    function encode(matrix) {
+        var result = "";
+        var line = 1;
+        if (matrix.dimension != 2)
+            throw new Error("Matrix should be 2-dimensional to be saved as CSV.");
+        matrix.forEach(function (item, coordinate) {
+            if (coordinate[0] != line) {
+                result += "\r\n";
+                line++;
+            } else if (coordinate[1] != 1) {
+                result += ',';
+            }
+
+            if (item === undefined)
+                return;
+            item = "" + item;
+            if (item.match(/[,\"]/))
+                result += escape(item);
+            else
+                result += item;
+        });
+        return result;
+    }
+    CSV.encode = encode;
+    function escape(input) {
+        return '"' + input.replace(/\"/g, '""') + '"';
+    }
+})(CSV || (CSV = {}));
+//# sourceMappingURL=matrixcsv.js.map
