2a57eb7611c2ec34ee54518b953abe0721068849
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-05-16 20:39:47 +0900

    ignore built

--- built/matrixcsv.d.ts
+++
@@ -1,5 +0,0 @@
-declare module MatrixCSV {
-    function readFile(file: Blob, delimiter?: string): Promise<Matrix<string>>;
-    function decode(text: string, delimiter?: string): Matrix<string>;
-    function encode(matrix: Matrix<any>): string;
-}
